8fc6d7bdbce12e91d20091e93240246cd6a497fe
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2020-07-31 13:36:05 -0300

    Adicionado CORS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/files', express.static(path.resolve(__dirname, "../temp/uploads")));
+app.use("/files", express.static(path.resolve(__dirname, "../temp/uploads")));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-app.listen(3000);
+app.listen(process.env.PORT);
